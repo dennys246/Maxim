16acc6340c8ecd601091c7cce765c1382135f113
Denny Schaedig <dennys25@pm.me>
2025-12-28 14:25:47 -0700

    Adding in daemon namespace instead of Maxim

--- src/conscience/selfy.py
+++ src/conscience/selfy.py
@@ -1,6 +1,7 @@
-import math, time
+import time
+from typing import Optional
+
 from reachy_mini import ReachyMini
-import numpy as np
 
 from src.motion.movement import move_head
 
@@ -9,40 +10,76 @@ class Maxim:
     Reachy-Mini modality threader for distributed computing
     """
 
-    def __init__(self,
-                 reachy_ip = "192.168.50.149",
-                 robot_name = "Maxim",
-                 timeout = 30.0):
+    def __init__(
+        self,
+        reachy_ip: str = "192.168.50.149",
+        robot_name: str = "reachy_mini",
+        timeout: float = 30.0,
+    ):
         self.alive = True
 
         self.name = robot_name
         self.start = time.time()
-        self.__reachy_ip = reachy_ip
+        self._reachy_ip = reachy_ip
+        self.duration = 1.0
 
+        # robot_name must match the daemon namespace (default: reachy_mini).
+        # localhost_only=False enables zenoh peer discovery across the LAN.
         self.mini = ReachyMini(
-            robot_name = self.name,
-            localhost_only = False,
-            timeout = timeout)
+            robot_name=self.name,
+            localhost_only=False,
+            spawn_daemon=False,
+            use_sim=False,
+            timeout=timeout,
+        )
 
-        self.x = 0
-        self.y = 0
-        self.z = 0
+        self.x = 0.0
+        self.y = 0.0
+        self.z = 0.0
 
-        self.roll = 0
-        self.pitch = 0
-        self.yaw = 0
+        self.roll = 0.0
+        self.pitch = 0.0
+        self.yaw = 0.0
     
     def live(self):
-        for x in [-1, -2, -4, 4]:
-            self.move(x = x)
-        return
+        for x in (-20, 0, 20, 0):
+            self.move(x=x, duration=0.8)
 
-    def move(self, x = None, y = None, z = None, roll = None, pitch = None, yaw = None, duration = 1.0):
-        for param, class_param in zip([x, y, z, roll, pitch, yaw], [self.x, self.y, self.z, self.roll, self.pitch, self.yaw]):
-            if param and param != class_param:
-                class_param = param
+    def move(
+        self,
+        x: Optional[float] = None,
+        y: Optional[float] = None,
+        z: Optional[float] = None,
+        roll: Optional[float] = None,
+        pitch: Optional[float] = None,
+        yaw: Optional[float] = None,
+        duration: Optional[float] = None,
+    ) -> None:
+        if duration is not None:
+            self.duration = duration
 
-        move_head(self.mini, self.x, self.y, self.z, self.roll, self.pitch, self.yaw, self.duration)
+        updates = {
+            "x": x,
+            "y": y,
+            "z": z,
+            "roll": roll,
+            "pitch": pitch,
+            "yaw": yaw,
+        }
+        for attr, val in updates.items():
+            if val is not None:
+                setattr(self, attr, val)
+
+        move_head(
+            self.mini,
+            self.x,
+            self.y,
+            self.z,
+            self.roll,
+            self.pitch,
+            self.yaw,
+            self.duration,
+        )
 
     def hear(self):
         return
