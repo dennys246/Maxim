7ab7f5360fb65f308b59bf4ddb3605d441ee590c
Denny Schaedig <dennys@wustl.edu>
2026-01-17 14:10:00 -0700

    Adding in language model helpers

--- src/maxim/models/language/__init__.py
+++ src/maxim/models/language/__init__.py
@@ -5,7 +5,29 @@ This package provides lightweight, swappable backends for local LLM routing.
 
 from __future__ import annotations
 
-from maxim.models.language.router import LLMRouter
+from maxim.models.language.router import (
+    DEFAULT_QUANTIZATION,
+    LLMConfig,
+    LLMRouter,
+    QUANTIZATION_LEVELS,
+    build_model_path,
+    list_llm_profiles,
+    list_prompt_styles,
+    list_quantization_levels,
+    load_llm_config,
+    normalize_llm_profile,
+)
 
-__all__ = ["LLMRouter"]
+__all__ = [
+    "DEFAULT_QUANTIZATION",
+    "LLMConfig",
+    "LLMRouter",
+    "QUANTIZATION_LEVELS",
+    "build_model_path",
+    "list_llm_profiles",
+    "list_prompt_styles",
+    "list_quantization_levels",
+    "load_llm_config",
+    "normalize_llm_profile",
+]
 
